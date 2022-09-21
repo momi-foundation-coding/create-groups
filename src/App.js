24afe4078af0ecb9165268ef5b31faa800f25e92
Boaz <boaserem022@gmail.com>
2022-09-21 17:15:44 +0300

    randomize data array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
+import React from "react";
+import {data} from "./data";
 
-function App() {
+
+export const  App = () => {
+
+  const arrIndex = [];
+  // const noMembers= 0;
+  // const groupSize = data.length/noMembers;
+  
+  const arrIndexGenetator = ()=>{
+    for(var i =0; i < data.length; i++) {
+      arrIndex.push(i);
+    }
+  //  
+  }
+
+  const Randomize = () => {
+    arrIndexGenetator();
+
+    data.map((datum, index, data)=>{
+
+      for(var a =0; a < data.length; a++) {
+        if(arrIndex.indexOf(datum.id) !== -1) {
+      
+          datum.id = Math.floor(Math.random() * data.length);
+          arrIndex.pop(datum.id);
+        }
+      }
+      return data;
+
+    })
+  };
+
+  console.log(data);
+
   return (
     <div >
-    React works in netlify
-      
+    
+        <h1>Please input your required number per group:</h1>
+
+        <label  >Number per group</label>
+
+        <input type="number" name="numberPerGroup"  required/>
+
+
+        <button onClick={Randomize }>Randomize</button>
+        
+        <div>
+
+        </div>
     </div>
+    
   );
 }
 
-export default App;
+
